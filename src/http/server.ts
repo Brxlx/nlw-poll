a5d788ff649e7b58bc60d8dccd1b07841a229165
Bruno Santos <brcesar19@gmail.com>
2024-02-06 11:44:15 -0300

    feat: add prisma

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,7 +1,29 @@
-import fastify from 'fastify';
+import fastify, { FastifyReply, FastifyRequest } from 'fastify';
+import { PrismaClient } from '@prisma/client';
+import { z } from 'zod';
+
 
 const server = fastify();
 
+const prisma = new PrismaClient();
+
+server.post('/polls', async (req: FastifyRequest, res: FastifyReply) => {
+  const createPollBody = z.object({
+    title: z.string().min(5)
+  });
+
+  const { title } = createPollBody.parse(req.body);
+
+  // const poll = await prisma.poll.create({
+  //   data: {
+  //     title
+  //   }
+  // });
+
+  return res.status(201).header('poll-id', 'as8as8as7d').send();
+});
+
+
 server.listen({ host: '0.0.0.0', port: 3333 }, () => {
   console.log('HTTP server is up and running!');
-})
+});

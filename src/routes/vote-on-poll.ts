01f40a1e9cdac3c2512057c33cde96eaa1e0624f
Bruno Santos <brcesar19@gmail.com>
2024-02-09 11:37:58 -0300

    chore: check if voted on the option on the poll

--- src/routes/vote-on-poll.ts
+++ src/routes/vote-on-poll.ts
@@ -20,10 +20,33 @@ export async function voteOnPoll(server: FastifyInstance) {
 
     let { session_id } = req.cookies;
 
+    if (session_id) {
+      const userPreviousVotedOnPoll = await prisma.vote.findUnique({
+        where: {
+          session_id_poll_id: {
+            session_id,
+            poll_id
+          }
+        }
+      });
+
+      if (userPreviousVotedOnPoll && userPreviousVotedOnPoll.poll_option_id !== poll_option_id) {
+        // Apagar o voto anterior
+        await prisma.vote.delete({
+          where: {
+            id: userPreviousVotedOnPoll.id
+          }
+        });
+      } else if (userPreviousVotedOnPoll) {
+        // Já votou nesta mesma opção
+        return res.status(400).send({ message: "You already voted on this option on this poll." });
+      }
+    }
+
     if (!session_id) {
       session_id = randomUUID();
 
-      res.setCookie('session-id', session_id, {
+      res.setCookie('session_id', session_id, {
         path: '/',
         maxAge: 60 * 60 * 24 * 30, // 30 days,
         signed: true,
@@ -31,7 +54,15 @@ export async function voteOnPoll(server: FastifyInstance) {
       });
     }
 
+    await prisma.vote.create({
+      data: {
+        session_id,
+        poll_id,
+        poll_option_id
+      }
+    })
 
-    return res.status(201).send({ session_id });
+
+    return res.status(201).send();
   });
 }
